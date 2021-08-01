68e3acbd2e500d9b695fb569e0ce6268cdea8b59
Arthri <41360489+Arthri@users.noreply.github.com>
2021-08-01 18:42:15 +0800

    feat: Add plugin

---
+++ src/SelfKickPlugin/SelfKick.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace SelfKickPlugin
+{
+    [ApiVersion(2, 1)]
+    public class SelfKick : TerrariaPlugin
+    {
+        public override string Name => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title;
+
+        public override string Description => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public override string Author => Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company;
+
+        public SelfKick(Main game) : base(game)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            Console.WriteLine($"    {Name}  Copyright (C) 2021  {Author}");
+            Console.WriteLine($"    This program comes with ABSOLUTELY NO WARRANTY; see {Name}.COPYING for details");
+            Console.WriteLine($"    This is free software, and you are welcome to redistribute it");
+            Console.WriteLine($"    under certain conditions; see {Name}.COPYING for details.");
+        }
+    }
+}
